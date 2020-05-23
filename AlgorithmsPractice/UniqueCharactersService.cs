58bd38e72206c1f5d69cfd91d6cb896eb690d0b9
EmmaLeu <manuela.leu@uipath.com>
2020-05-23 16:04:28 +0300

    Determine a string has unique characters problem

---
+++ AlgorithmsPractice/UniqueCharactersService.cs
@@ -0,0 +1,83 @@
+﻿using System.Linq;
+
+namespace AlgorithmsPractice
+{
+    /// <summary>
+    /// We assume we have only alphanumeric characters with ASCII between 0..255
+    /// and that lower case letter is diffrent than upper case letter
+    /// </summary>
+    public static class UniqueCharactersService
+    {
+        public static bool HasUniqueCharactersFrequency(string input) // Time O(input.Length); Space O(256)
+        {
+            if(string.IsNullOrEmpty(input))
+            {
+                return true;
+            }
+
+            var frequencyArray = new int[256];
+
+            foreach(var character in input)
+            {
+                frequencyArray[character]++;
+                if(frequencyArray[character] > 1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool HasUniqueCharactersBruteforce(string input) // O(input.Length^2)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return true;
+            }
+
+            if(input.Length == 1)
+            {
+                return true;
+            }
+
+            for(var index = 0; index < input.Length - 1; index ++)
+            {
+                for(var otherIndex = index + 1; otherIndex < input.Length; otherIndex++)
+                {
+                    if(input[index] == input[otherIndex])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public static bool HasUniqueCharactersSorting(string input) // Time O(input.Length * log(input.Length)); Space: O(input.Length)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return true;
+            }
+
+            if (input.Length == 1)
+            {
+                return true;
+            }
+
+            var sortedInput = input.OrderBy(c => c).ToArray();
+
+            for (var index = 0; index < sortedInput.Length - 1; index++)
+            { 
+                if(sortedInput[index] == sortedInput[index + 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ AlgorithmsPracticeTests/UniqueCharactersServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using AlgorithmsPractice;
+using NUnit.Framework;
+
+namespace AlgorithmsPracticeTests
+{
+    public class UniqueCharactersServiceTests
+    {
+        [TestCase(null, true)]
+        [TestCase("1", true)]
+        [TestCase("1ABcae", true)]
+        [TestCase("11ABcae", false)]
+        [TestCase("1sasabsabsbaaaa", false)]
+        public void UniqueCharactersService_HasUniqueCharactersFrequency_Test(string input, bool expectedOutput)
+        {
+            var result = UniqueCharactersService.HasUniqueCharactersFrequency(input);
+
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [TestCase(null, true)]
+        [TestCase("1", true)]
+        [TestCase("1ABcae", true)]
+        [TestCase("11ABcae", false)]
+        [TestCase("1sasabsabsbaaaa", false)]
+        public void UniqueCharactersService_HasUniqueCharactersBruteforce_Test(string input, bool expectedOutput)
+        {
+            var result = UniqueCharactersService.HasUniqueCharactersBruteforce(input);
+
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [TestCase(null, true)]
+        [TestCase("1", true)]
+        [TestCase("1ABcae", true)]
+        [TestCase("11ABcae", false)]
+        [TestCase("1sasabsabsbaaaa", false)]
+        public void UniqueCharactersService_HasUniqueCharactersSorting_Test(string input, bool expectedOutput)
+        {
+            var result = UniqueCharactersService.HasUniqueCharactersSorting(input);
+
+            Assert.AreEqual(expectedOutput, result);
+        }
+    }
+}
