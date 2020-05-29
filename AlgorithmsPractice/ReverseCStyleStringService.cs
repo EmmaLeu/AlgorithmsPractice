9ce16c1b59f5984c2462238b3ab16ef653b9a0cf
EmmaLeu <manuela.leu@uipath.com>
2020-05-29 10:45:31 +0300

    Add reverse C-Style string problem

---
+++ AlgorithmsPractice/ReverseCStyleStringService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace AlgorithmsPractice
+{
+    public class ReverseCStyleStringService
+    {
+        public const string NullUnicode = "\0";
+
+        public static string ReverseStringUsingStringBuilder(string CStyleString)
+        {
+            if(string.IsNullOrEmpty(CStyleString))
+            {
+                throw new ArgumentNullException(nameof(CStyleString));
+            }
+
+            if (CStyleString == NullUnicode)
+            {
+                return CStyleString;
+            }
+
+            var reversedString = new StringBuilder();
+            for(var charIndex = CStyleString.Length - 1; charIndex >= 0; charIndex--)
+            {
+                reversedString.Append(CStyleString[charIndex]);
+            }
+
+            return reversedString.ToString();
+        }
+    }
+}
---
+++ AlgorithmsPracticeTests/ReverseCStyleStringServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using AlgorithmsPractice;
+using NUnit.Framework;
+
+namespace AlgorithmsPracticeTests
+{
+    public class ReverseCStyleStringServiceTests
+    {
+        [TestCase("1\0", "\01")]
+        [TestCase("yaya\0", "\0ayay")]
+        [TestCase("\0", "\0")]
+        public void ReverseCStyleStringService_ReverseString_Test(string input, string expectedOutput)
+        {
+            var result = ReverseCStyleStringService.ReverseStringUsingStringBuilder(input);
+
+            Assert.AreEqual(expectedOutput, result);
+        }
+    }
+}
