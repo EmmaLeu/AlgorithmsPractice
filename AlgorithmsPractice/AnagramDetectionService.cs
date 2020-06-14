5ad3eaf95538169c2976812d0b61b20319f931e9
EmmaLeu <manuela.leu@uipath.com>
2020-06-14 19:23:13 +0300

    Determine if 2 strings are anagrams

---
+++ AlgorithmsPractice/AnagramDetectionService.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgorithmsPractice
+{
+    /// <summary>
+    /// Assuming we have only alphanumeric characters with ASCII between 0..255
+    /// and that lower case letter is same as upper case
+    /// </summary>
+    public class AnagramDetectionService
+    {
+        //O(nlogn) complexity because of sorting
+        public static bool AreAnagrams_Sorting(string firstString, string secondString)
+        {
+            if(string.IsNullOrEmpty(firstString) && string.IsNullOrEmpty(secondString))
+            {
+                return true;
+            }
+
+            if(firstString == null || secondString == null)
+            {
+                return false;
+            }
+
+            if(firstString.Length != secondString.Length)
+            {
+                return false;
+            }
+
+            var orderedFirstString = firstString.OrderBy(s => s).ToString();
+            var orderedSecondString = secondString.OrderBy(s => s).ToString();
+
+            if (orderedFirstString.Equals(orderedSecondString, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return true;
+            }
+            
+            return false;
+        }
+
+        //O(n) complexity
+        public static bool AreAnagrams_Frequency(string firstString, string secondString)
+        {
+            if (string.IsNullOrEmpty(firstString) && string.IsNullOrEmpty(secondString))
+            {
+                return true;
+            }
+
+            if (firstString == null || secondString == null)
+            {
+                return false;
+            }
+
+            if (firstString.Length != secondString.Length)
+            {
+                return false;
+            }
+
+            var frequencyArray = new int[256];
+
+            foreach(var character in firstString)
+            {
+                 frequencyArray[char.ToLower(character)]++;
+            }
+            
+            foreach(var character in secondString)
+            {
+                frequencyArray[char.ToLower(character)]--;
+            }
+
+            foreach(var character in frequencyArray)
+            {
+                if(character != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ AlgorithmsPracticeTests/AnagramDetectionServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using AlgorithmsPractice;
+using NUnit.Framework;
+
+namespace AlgorithmsPracticeTests
+{
+    public class AnagramDetectionServiceTests
+    {
+        [TestCase(null, null, true)]
+        [TestCase("", "", true)]
+        [TestCase("", null, true)] //consider null or empty string anagrams
+        [TestCase("aba", "baa", true)]
+        [TestCase(null, "a", false)]
+        [TestCase("a", null, false)]
+        [TestCase("aba", "baaa", false)]
+        public void AnagramDetectionService_AreAnagramsSorting_Test(string firstString, string secondString, bool shouldBeAnagrams)
+        {
+            var result = AnagramDetectionService.AreAnagrams_Sorting(firstString, secondString);
+
+            Assert.AreEqual(shouldBeAnagrams, result);
+        }
+
+        [TestCase(null, null, true)]
+        [TestCase("", "", true)]
+        [TestCase("", null, true)] //consider null or empty string anagrams
+        [TestCase("aba", "baa", true)]
+        [TestCase(null, "a", false)]
+        [TestCase("a", null, false)]
+        [TestCase("aba", "baaa", false)]
+        public void AnagramDetectionService_AreAnagramsFrequency_Test(string firstString, string secondString, bool shouldBeAnagrams)
+        {
+            var result = AnagramDetectionService.AreAnagrams_Frequency(firstString, secondString);
+
+            Assert.AreEqual(shouldBeAnagrams, result);
+        }
+    }
+}
