04416abca8416e38688c623f20d859cded91c35a
EmmaLeu <manuela.leu@uipath.com>
2020-06-22 16:08:02 +0300

    Rotate a NxN matrix image by 90 degrees

---
+++ AlgorithmsPractice/MatrixRotationService.cs
@@ -0,0 +1,67 @@
+﻿namespace AlgorithmsPractice
+{
+
+    /// <summary>
+    /// Rotate a NxN matrix by 90 degrees
+    /// </summary>
+    public class MatrixRotationService
+    {
+        public static void RotateInPlace(int[][] matrix)
+        {
+            if (matrix == null)
+            {
+                return;
+            }
+
+            var length = matrix.GetLength(0);
+
+            for (var layer = 0; layer < length / 2; ++layer)
+            {
+                var first = layer;
+                var last = length - 1 - layer;
+                for (var index = first; index < last; ++index)
+                {
+                    var offset = index - first;
+                    var top = matrix[first][index];
+                    matrix[first][index] = matrix[last - offset][first]; //left -> top
+                    matrix[last - offset][first] = matrix[last][last - offset]; //bottom -> left
+                    matrix[last][last - offset] = matrix[index][last]; // right -> bottom
+                    matrix[index][last] = top; //top -> right
+                }
+            }
+        }
+
+        public static int[][] RotateUsingAdditionalMemory(int[][] matrix)
+        {
+            if (matrix == null)
+            {
+                return null;
+            }
+
+            var length = matrix.GetLength(0);
+            int[][] rotatedMatrix = GetInitializedRotatedMatrix(length);
+
+            for (var lineIndex = 0; lineIndex < length; lineIndex++)
+            {
+                for (var columnIndex = 0; columnIndex < length; columnIndex++)
+                {
+                    rotatedMatrix[columnIndex][length - lineIndex - 1] = matrix[lineIndex][columnIndex];
+                }
+            }
+
+            return rotatedMatrix;
+        }
+
+        private static int[][] GetInitializedRotatedMatrix(int length)
+        {
+            var rotatedMatrix = new int[length][];
+
+            for (var lineIndex = 0; lineIndex < length; lineIndex++)
+            {
+                rotatedMatrix[lineIndex] = new int[length];
+            }
+
+            return rotatedMatrix;
+        }
+    }
+}
---
+++ AlgorithmsPracticeTests/MatrixRotationServiceTests.cs
@@ -0,0 +1,65 @@
+﻿using AlgorithmsPractice;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace AlgorithmsPracticeTests
+{
+    public class MatrixRotationServiceTests
+    {
+        [TestCaseSource(nameof(TestCaseData))]
+        public void MatrixRotationService_Rotate_Test(int[][] input, int[][] expected)
+        {
+            var actual = MatrixRotationService.RotateUsingAdditionalMemory(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCaseSource(nameof(TestCaseData))]
+        public void MatrixRotationService_RotateInPlace_Test(int[][] input, int[][] expected)
+        {
+            MatrixRotationService.RotateInPlace(input);
+
+            Assert.AreEqual(expected, input);
+        }
+
+        public static IEnumerable<object[]> TestCaseData
+        {
+            get
+            {
+                yield return new object[]
+                {
+                    new int[][] //input
+                    {
+                        new int[] { 1, 2, 3 },
+                        new int[] { 4, 5, 6 },
+                        new int[] { 7, 8, 9}
+                    },
+                    new int[][] //expected
+                    {
+                        new int[] { 7, 4, 1 },
+                        new int[] { 8, 5, 2 },
+                        new int[] { 9, 6, 3}
+                    }
+                };
+
+                yield return new object[]
+                {
+                    new int[][] //input
+                    {
+                        new int[] { 1 }
+                    },
+                    new int[][] //expected
+                    {
+                        new int[] { 1 }
+                    }
+                };
+
+                yield return new object[]
+                {
+                    null,
+                    null
+                };
+            }
+        }
+    }
+}
