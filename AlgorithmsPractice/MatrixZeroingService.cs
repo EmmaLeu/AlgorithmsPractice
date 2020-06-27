ae3a775acc9564573d7ad339128fdb9e5f6b514c
EmmaLeu <manuela.leu@uipath.com>
2020-06-27 23:48:46 +0300

    Set column and line to zero when a zero is found in a matrix

---
+++ AlgorithmsPractice/MatrixZeroingService.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace AlgorithmsPractice
+{
+    ///<summary>
+    ///Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column is set to 0 
+    ///O(lineLengthxcolumnLength) time
+    ///Auxiliary O(lineLength) + O(columnLength) space
+    ///</summary>
+    public class MatrixZeroingService
+    {
+        public static void ZeroMartrix(int[][] matrix, int lineLength, int columnLength)
+        {
+            if (matrix == null)
+            {
+                return;
+            }
+
+            var columnsToZero = new HashSet<int>();
+            var linesToZero = new HashSet<int>();
+            for (var lineIndex = 0; lineIndex < lineLength; lineIndex++)
+            {
+                for (var columnIndex = 0; columnIndex < columnLength; columnIndex++)
+                {
+                    if (matrix[lineIndex][columnIndex] == 0)
+                    {
+                        columnsToZero.Add(columnIndex);
+                        linesToZero.Add(lineIndex);
+                    }
+                }
+            }
+
+            SetColumnsToZero(matrix, lineLength, columnsToZero);
+            SetLinesToZero(matrix, columnLength, linesToZero);
+        }
+
+        private static void SetLinesToZero(int[][] matrix, int columnLength, HashSet<int> linesToZero)
+        {
+            foreach (var lineIndex in linesToZero)
+            {
+                for (var columnIndex = 0; columnIndex < columnLength; columnIndex++)
+                {
+                    matrix[lineIndex][columnIndex] = 0;
+                }
+            }
+        }
+
+        private static void SetColumnsToZero(int[][] matrix, int lineLength, HashSet<int> columnsToZero)
+        {
+            foreach (var columnIndex in columnsToZero)
+            {
+                for (var lineIndex = 0; lineIndex < lineLength; lineIndex++)
+                {
+                    matrix[lineIndex][columnIndex] = 0;
+                }
+            }
+        }
+    }
+}
---
+++ AlgorithmsPracticeTests/MatrixZeroingServiceTests.cs
@@ -0,0 +1,69 @@
+﻿using AlgorithmsPractice;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace AlgorithmsPracticeTests
+{
+    public class MatrixZeroingServiceTests
+    {
+        [TestCaseSource(nameof(TestCaseData))]
+        public void MatrixZeroingService_ZeroMarix_Test(int[][] input, int lines, int columns, int[][] expected)
+        {
+            MatrixZeroingService.ZeroMartrix(input, lines, columns);
+
+            Assert.AreEqual(expected, input);
+        }
+
+        public static IEnumerable<object[]> TestCaseData
+        {
+            get
+            {
+                yield return new object[]
+                {
+                    new int[][] //input
+                    {
+                        new int[] { 7, 4, 3 },
+                        new int[] { 4, 5, 0 },
+                        new int[] { 7, 8, 0}
+                    },
+                    3,
+                    3,
+                    new int[][] //expected
+                    {
+                        new int[] { 7, 4, 0 },
+                        new int[] { 0, 0, 0 },
+                        new int[] { 0, 0, 0}
+                    }
+                };
+
+                yield return new object[]
+                {
+                    new int[][] //input
+                    {
+                        new int[] { 1, 1, 0 },
+                        new int[] { 1, 1, 0 },
+                        new int[] { 1, 1, 0 },
+                        new int[] { 1, 1, 0 }
+                    },
+                    4,
+                    3,
+                    new int[][] //expected
+                    {
+                        new int[] { 0, 0, 0 },
+                        new int[] { 0, 0, 0 },
+                        new int[] { 0, 0, 0 },
+                        new int[] { 0, 0, 0 }
+                    }
+                };
+
+                yield return new object[]
+                {
+                    null,
+                    0,
+                    0,
+                    null
+                };
+            }
+        }
+    }
+}
