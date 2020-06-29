0cc78d65abce8f33650110c2f41a2947e6e2d061
EmmaLeu <manuela.leu@uipath.com>
2020-06-29 22:42:48 +0300

    Find if s2 is rotation of s1 using only one contains call

---
+++ AlgorithmsPractice/StringRotationService.cs
@@ -0,0 +1,31 @@
+﻿namespace AlgorithmsPractice
+{
+    /// <summary>
+    /// Assume you have a method isSubstring which checks if one word is a substring of another   
+    /// Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 
+    /// using only one call to isSubstring (i e , “waterbottle” is a rotation of “erbottlewat”) 
+    /// </summary>
+    public class StringRotationService
+    {
+        public static bool IsRotation(string s1, string s2)
+        {
+            if(string.IsNullOrEmpty(s1) && string.IsNullOrEmpty(s2))
+            {
+                return true;
+            }
+
+            if(s1.Length != s2.Length)
+            {
+                return false;
+            }
+
+            var s1s1 = s1 + s1;
+            if(s1s1.Contains(s2))
+            {
+                return true;
+            }
+            
+            return false;
+        }
+    }
+}
---
+++ AlgorithmsPracticeTests/StringRotationServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using AlgorithmsPractice;
+using NUnit.Framework;
+
+namespace AlgorithmsPracticeTests
+{
+    public class StringRotationServiceTests
+    {
+        [TestCase("", "", true)]
+        [TestCase(null, null, true)]
+        [TestCase("a","a", true)]
+        [TestCase("aba", "aba", true)]
+        [TestCase("waterbottle", "erbottlewat", true)]
+        [TestCase("aba", "a", false)]
+        public void StringRotationService_IsRotation_Test(string s1, string s2, bool isRotation)
+        {
+            var result = StringRotationService.IsRotation(s1, s2);
+            Assert.AreEqual(isRotation, result);
+        }
+    }
+}
