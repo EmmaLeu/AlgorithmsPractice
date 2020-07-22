e82c9b60393a598a48da77ef99274c260d4b0a88
EmmaLeu <manuela.leu@uipath.com>
2020-07-23 01:33:38 +0300

    Implement 3 stacks using an array

---
+++ AlgorithmsPractice/StacksAndQueues/ThreeStackService.cs
@@ -0,0 +1,50 @@
+﻿namespace AlgorithmsPractice.StacksAndQueues
+{
+    /// <summary>
+    /// Implement 3 stacks using only one array (assuming we have fixed size stacks)
+    /// </summary>
+    public class ThreeStackService
+    {
+        private readonly int _stackSize;
+        private readonly int[] _buffer;
+        private readonly int[] _topElementPointers = { 0, 0, 0 };
+
+        public ThreeStackService(int stackSize)
+        {
+            _stackSize = stackSize;
+            _buffer = new int[stackSize * 3];
+        }
+
+        public void Push(int stackNo, int value)
+        {
+            var index = GetTopIndex(stackNo) + 1;
+            _topElementPointers[stackNo]++;
+            _buffer[index] = value;
+        }
+
+        public int Pop(int stackNo)
+        {
+            var index = GetTopIndex(stackNo);
+            _topElementPointers[stackNo]--;
+            int value = _buffer[index];
+            _buffer[index] = 0;
+            return value;
+        }
+
+        public int Peek(int stackNo)
+        {
+            var index = GetTopIndex(stackNo);
+            return _buffer[index];
+        }
+
+        public bool IsEmpty(int stackNumber)
+        {
+            return _topElementPointers[stackNumber] == _stackSize * stackNumber;
+        }
+
+        private int GetTopIndex(int stackNo)
+        {
+            return stackNo * _stackSize + _topElementPointers[stackNo];
+        }
+    }
+}
---
+++ AlgorithmsPracticeTests/StacksAndQueues/ThreeStackServiceTest.cs
@@ -0,0 +1,54 @@
+﻿using AlgorithmsPractice.StacksAndQueues;
+using NUnit.Framework;
+
+namespace AlgorithmsPracticeTests.StacksAndQueues
+{
+    public class ThreeStackServiceTest
+    {
+        [Test]
+        public void Peek_Test()
+        {
+            var service = new ThreeStackService(3);
+
+            service.Push(0, 1);
+            service.Push(1, 2);
+            service.Push(2, 3);
+
+            var firstStackValue = service.Peek(0);
+            Assert.AreEqual(1, firstStackValue);
+
+            var secondStackValue = service.Peek(1);
+            Assert.AreEqual(2, secondStackValue);
+
+            var thirdStackValue = service.Peek(2);
+            Assert.AreEqual(3, thirdStackValue);
+
+            Assert.False(service.IsEmpty(0));
+            Assert.False(service.IsEmpty(1));
+            Assert.False(service.IsEmpty(2));
+        }
+
+        [Test]
+        public void Pop_Test()
+        {
+            var service = new ThreeStackService(9);
+
+            service.Push(0, 1);
+            service.Push(1, 2);
+            service.Push(2, 3);
+
+            var firstStackValue = service.Pop(0);
+            Assert.AreEqual(1, firstStackValue);
+
+            var secondStackValue = service.Pop(1);
+            Assert.AreEqual(2, secondStackValue);
+
+            var thirdStackValue = service.Pop(2);
+            Assert.AreEqual(3, thirdStackValue);
+
+            Assert.True(service.IsEmpty(0));
+            Assert.True(service.IsEmpty(1));
+            Assert.True(service.IsEmpty(2));
+        }
+    }
+}
