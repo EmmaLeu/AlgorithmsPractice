305905fd99bb9099c445809b66deeddb796e048c
EmmaLeu <manuela.leu@uipath.com>
2020-08-02 13:58:15 +0300

    Implement a stack that also has a Min() method in O(1)

---
+++ AlgorithmsPractice/StacksAndQueues/StackWithMin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+
+namespace AlgorithmsPractice.StacksAndQueues
+{
+    public class StackWithMin : Stack
+    {
+        public void Push(int value) 
+        { 
+            int newMin = Math.Min(value, Min()); 
+            Push(new NodeWithMin(value, newMin));
+        }
+
+        public int Min() 
+        {
+            if (Count == 0) 
+            {
+                return int.MaxValue;
+            } 
+
+            return (Peek() as NodeWithMin).Min;
+        }
+    }
+
+    /// <summary>
+    /// Node holding current min information
+    /// </summary>
+    public class NodeWithMin
+    {
+        public int Min;
+        public int Value;
+
+        public NodeWithMin(int value, int min)
+        {
+            Value = value;
+            Min = min;
+        }
+    }
+}
+
---
+++ AlgorithmsPracticeTests/StacksAndQueues/StackWithMinTests.cs
@@ -0,0 +1,21 @@
+﻿using AlgorithmsPractice.StacksAndQueues;
+using NUnit.Framework;
+
+namespace AlgorithmsPracticeTests.StacksAndQueues
+{
+    public class StackWithMinTests
+    {
+        [Test]
+        public void Min_Test()
+        {
+            var stack = new StackWithMin();
+
+            stack.Push(3);
+            stack.Push(2);
+            stack.Push(8);
+            stack.Push(1);
+
+            Assert.AreEqual(1, stack.Min());
+        }
+    }
+}
