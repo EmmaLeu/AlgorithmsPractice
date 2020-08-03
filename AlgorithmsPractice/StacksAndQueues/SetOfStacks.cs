7b25df8a9ff35ef4368404a8ad74b6fe6c5b4786
EmmaLeu <manuela.leu@uipath.com>
2020-08-04 00:24:37 +0300

    Implement a stack using multiple stacks created when a threshold is reached

---
+++ AlgorithmsPractice/StacksAndQueues/SetOfStacks.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmsPractice.StacksAndQueues
+{
+    /// <summary>
+    /// SetOfStacks should be composed of several stacks, and should create a new stack once the previous one exceeds capacity   
+    /// push() and pop() behave identically to a single stack
+    /// </summary>
+    public class SetOfStacks
+    {
+        private readonly int _threshold;
+        private readonly List<Stack> _stacks = new List<Stack>();
+
+        public SetOfStacks(int threshold)
+        {
+            _threshold = threshold;
+        }
+
+        public void Push(object value)
+        {
+            var stack = GetPushStack();
+            stack.Push(value);
+        }
+
+        public object Pop()
+        {
+            var stack = GetPopStack();
+            if(stack == null || stack.Count == 0)
+            {
+                return null;
+            }
+
+            return stack.Pop();
+        }
+
+        public int GetStackNumber() => _stacks.Count;
+
+        private Stack GetPushStack()
+        {
+            var stack = _stacks.LastOrDefault();
+            if (stack != null && stack.Count < _threshold)
+            {
+                return stack;
+            }
+
+            stack = new Stack();
+            _stacks.Add(stack);
+            return stack;
+        }
+
+        private Stack GetPopStack()
+        {
+            var stack = _stacks.LastOrDefault();
+            if(stack == null)
+            {
+                return null;
+            }
+
+            if(stack.Count <= 1)
+            {
+                _stacks.Remove(stack);
+            }
+
+            return stack;
+        }
+    }
+}
---
+++ AlgorithmsPracticeTests/StacksAndQueues/SetOfStacksTests.cs
@@ -0,0 +1,49 @@
+﻿using AlgorithmsPractice.StacksAndQueues;
+using NUnit.Framework;
+
+namespace AlgorithmsPracticeTests.StacksAndQueues
+{
+    public class SetOfStacksTests
+    {
+        [Test]
+        public void Init_Test()
+        {
+            var stack = new SetOfStacks(0);
+            Assert.AreEqual(0, stack.GetStackNumber());
+        }
+
+        [Test]
+        public void Push_Test()
+        {
+            var stack = new SetOfStacks(2);
+            stack.Push(1);
+            stack.Push(2);
+            Assert.AreEqual(1, stack.GetStackNumber());
+
+            stack.Push(3);
+            Assert.AreEqual(2, stack.GetStackNumber());
+        }
+
+
+        [Test]
+        public void Pop_Test()
+        {
+            var stack = new SetOfStacks(2);
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            var value = stack.Pop();
+            Assert.AreEqual(3, value);
+            Assert.AreEqual(1, stack.GetStackNumber());
+
+            value = stack.Pop();
+            Assert.AreEqual(2, value);
+            Assert.AreEqual(1, stack.GetStackNumber());
+
+            value = stack.Pop();
+            Assert.AreEqual(1, value);
+            Assert.AreEqual(0, stack.GetStackNumber());
+        }
+    }
+}
