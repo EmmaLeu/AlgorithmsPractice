a0f63c89630de4e1abe13eeea83a0fbf869ad023
EmmaLeu <manuela.leu@uipath.com>
2020-08-22 11:38:59 +0300

    Towers of Hanoi implementation using stacks

---
+++ AlgorithmsPractice/StacksAndQueues/TowerOfHanoi.cs
@@ -0,0 +1,46 @@
+﻿namespace AlgorithmsPractice.StacksAndQueues
+{
+    public class TowerOfHanoi
+    {
+        private readonly Stack<int> _disks = new Stack<int>();
+
+        public TowerOfHanoi(int index)
+        {
+            Index = index;
+        }
+
+        public int Index { get; }
+
+        public bool Add(int disk)
+        {
+            if(!_disks.IsEmpty() && _disks.Peek() <= disk)
+            {
+                return false;
+            }
+
+            _disks.Push(disk);
+            return true;
+        }
+
+        public void MoveTopTo(TowerOfHanoi tower)
+        {
+            var top = _disks.Pop();
+            tower.Add(top);
+        }
+
+        public void MoveDisks(int n, TowerOfHanoi destination, TowerOfHanoi buffer)
+        {
+            if(n > 0)
+            {
+                MoveDisks(n - 1, buffer, destination);
+                MoveTopTo(destination);
+                buffer.MoveDisks(n - 1, destination, this);
+            }
+        }
+
+        public int Pop()
+        {
+            return _disks.Pop();
+        }
+    }
+}
---
+++ AlgorithmsPracticeTests/StacksAndQueues/TowersOfHanoiTests.cs
@@ -0,0 +1,34 @@
+﻿using AlgorithmsPractice.StacksAndQueues;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace AlgorithmsPracticeTests.StacksAndQueues
+{
+    public class TowersOfHanoiTests
+    {
+        [Test]
+        public void MoveTowersToLastRod_Test()
+        {
+            var n = 5;
+            var towers = new TowerOfHanoi[n];
+
+            for(var i = 0; i < 3; i++)
+            {
+                towers[i] = new TowerOfHanoi(i);
+            }
+
+            for(var i = n - 1; i >= 0; i--)
+            {
+                towers[0].Add(i);
+            }
+
+            towers[0].MoveDisks(n, towers[2], towers[1]);
+
+            Assert.AreEqual(0, towers[2].Pop());
+            Assert.AreEqual(1, towers[2].Pop());
+            Assert.AreEqual(2, towers[2].Pop());
+            Assert.AreEqual(3, towers[2].Pop());
+            Assert.AreEqual(4, towers[2].Pop());
+        }
+    }
+}
