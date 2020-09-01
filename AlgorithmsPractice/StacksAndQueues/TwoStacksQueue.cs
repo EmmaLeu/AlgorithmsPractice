e86efadc22c91ba38126fa5ac92c592fbe24c221
EmmaLeu <manuela.leu@uipath.com>
2020-09-02 00:26:00 +0300

    Implement a queue using 2 stacks

---
+++ AlgorithmsPractice/StacksAndQueues/TwoStacksQueue.cs
@@ -0,0 +1,30 @@
+﻿namespace AlgorithmsPractice.StacksAndQueues
+{
+    public class TwoStacksQueue
+    {
+        private readonly Stack<int> _enqueueStack = new Stack<int>();
+        private readonly Stack<int> _dequeueStack = new Stack<int>();
+
+        public void Enqueue(int value)
+        {
+            _enqueueStack.Push(value);
+        }
+
+        public int Dequeue() //O(n), n - number of elements
+        {
+            MoveElements(_enqueueStack, _dequeueStack);
+            var value = _dequeueStack.Pop();
+            MoveElements(_dequeueStack, _enqueueStack);
+            return value;
+        }
+
+        private void MoveElements(Stack<int> _stack1, Stack<int> _stack2)
+        {
+            while (!_stack1.IsEmpty())
+            {
+                var element = _stack1.Pop();
+                _stack2.Push(element);
+            }
+        }
+    }
+}
---
+++ AlgorithmsPracticeTests/StacksAndQueues/TwoStacksQueueTests.cs
@@ -0,0 +1,22 @@
+﻿using AlgorithmsPractice.StacksAndQueues;
+using NUnit.Framework;
+
+namespace AlgorithmsPracticeTests.StacksAndQueues
+{
+    public class TwoStacksQueueTests
+    {
+        [Test]
+        public void Enqueue_Dequeue_Test()
+        {
+            var queue = new TwoStacksQueue();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+
+            Assert.AreEqual(1, queue.Dequeue());
+            Assert.AreEqual(2, queue.Dequeue());
+            Assert.AreEqual(3, queue.Dequeue());
+        }
+    }
+}
