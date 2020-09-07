588b803dde7cdbe709eacea9ca4f945f25847e67
EmmaLeu <manuela.leu@uipath.com>
2020-09-07 23:15:52 +0300

    Sort stack in ascending order

---
+++ AlgorithmsPractice/StacksAndQueues/StackSortingService.cs
@@ -0,0 +1,26 @@
+﻿namespace AlgorithmsPractice.StacksAndQueues
+{
+    /// <summary>
+    /// Sort stack in ascending order without making assumptions regarding the stack implementation
+    /// Use Pop/Push/Peek/IsEmpty
+    /// </summary>
+    public static class StackSortingService
+    {
+        public static Stack<int> Sort(Stack<int> stack)
+        {
+            var helpingStack = new Stack<int>();
+            while (!stack.IsEmpty())
+            {
+                var top = stack.Pop();
+                while(!helpingStack.IsEmpty() && helpingStack.Peek() > top)
+                {
+                    stack.Push(helpingStack.Pop());
+                }
+
+                helpingStack.Push(top);
+            }
+
+            return helpingStack;
+        }
+    }
+}
---
+++ AlgorithmsPracticeTests/StacksAndQueues/StackSortingServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using AlgorithmsPractice.StacksAndQueues;
+using NUnit.Framework;
+
+namespace AlgorithmsPracticeTests.StacksAndQueues
+{
+    public class StackSortingServiceTests
+    {
+        [Test]
+        public void Sort_Test()
+        {
+            var stack = new AlgorithmsPractice.StacksAndQueues.Stack<int>();
+            stack.Push(1);
+            stack.Push(5);
+            stack.Push(2);
+            stack.Push(4);
+            stack.Push(3);
+
+            var result = StackSortingService.Sort(stack);
+
+            Assert.AreEqual(5, result.Pop());
+            Assert.AreEqual(4, result.Pop());
+            Assert.AreEqual(3, result.Pop());
+            Assert.AreEqual(2, result.Pop());
+            Assert.AreEqual(1, result.Pop());
+        }
+    }
+}
