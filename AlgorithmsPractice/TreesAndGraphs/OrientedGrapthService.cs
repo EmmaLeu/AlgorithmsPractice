e14e371b35770a08eaea75fa3843f78212528bc9
EmmaLeu <manuela.leu@uipath.com>
2020-10-18 01:05:40 +0300

    Add DFS and BFS for oriented graph

---
+++ AlgorithmsPractice/TreesAndGraphs/OrientedGrapthService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsPractice.TreesAndGraphs
+{
+    public class OrientedGrapthService
+    {
+        private int _numberOfVertices;
+        private Dictionary<int, List<int>> _adjacencyList;
+
+        public OrientedGrapthService(int numberOfVertices)
+        {
+            _numberOfVertices = numberOfVertices;
+            _adjacencyList = new Dictionary<int, List<int>>();
+            for (var index = 0; index < _numberOfVertices; index++)
+            {
+                _adjacencyList[index] = new List<int>();
+            }
+        }
+
+        public void AddEdge(int fromVertex, int toVertex)
+        {
+            if(!_adjacencyList.ContainsKey(fromVertex))
+            {
+                throw new ArgumentException(nameof(fromVertex));
+            }
+
+            _adjacencyList[fromVertex].Add(toVertex);
+        }
+
+        public List<int> BreadthFirstSearch(int vertex)
+        {
+            var traversal = new List<int>();
+            var visited = new HashSet<int>();
+            visited.Add(vertex);
+
+            var queue = new Queue<int>();
+            queue.Enqueue(vertex);
+
+            while(queue.Count > 0)
+            {
+                vertex = queue.Dequeue();
+                traversal.Add(vertex);
+                foreach(var adjacentVertex in _adjacencyList[vertex])
+                {
+                    if (!visited.Contains(adjacentVertex))
+                    {
+                        visited.Add(adjacentVertex);
+                        queue.Enqueue(adjacentVertex);
+                    }
+                }
+            }
+
+            return traversal;
+        }
+
+        public List<int> DepthFirstSearch(int vertex)
+        {
+            var traversal = new List<int>();
+            var visited = new HashSet<int>();
+
+            DepthFirstSearchRecursive(vertex, visited, traversal);
+            return traversal;
+        }
+
+        private void DepthFirstSearchRecursive(int vertex, HashSet<int> visited, List<int> traversal)
+        {
+            visited.Add(vertex);
+            traversal.Add(vertex);
+            foreach(var adjacentVertex in _adjacencyList[vertex])
+            {
+                if(!visited.Contains(adjacentVertex))
+                {
+                    DepthFirstSearchRecursive(adjacentVertex, visited, traversal);
+                }
+            }
+        }
+    }
+}
---
+++ AlgorithmsPracticeTests/TreesAndGraphs/OrientedGraphServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using AlgorithmsPractice.TreesAndGraphs;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace AlgorithmsPracticeTests.TreesAndGraphs
+{
+    public class OrientedGraphServiceTests
+    {
+        [Test]
+        public void DFS()
+        {
+            var service = new OrientedGrapthService(4);
+            service.AddEdge(2, 0);
+            service.AddEdge(0, 2);
+            service.AddEdge(1, 2);
+            service.AddEdge(0, 1);
+            service.AddEdge(3, 3);
+            service.AddEdge(1, 3);
+            var expectedTraversal = new List<int>(new int[] { 2, 0, 1, 3 });
+
+            var result = service.DepthFirstSearch(2);
+
+            Assert.AreEqual(expectedTraversal, result);
+        }
+
+        [Test]
+        public void BFS()
+        {
+            var service = new OrientedGrapthService(4);
+            service.AddEdge(2, 0);
+            service.AddEdge(0, 2);
+            service.AddEdge(1, 2);
+            service.AddEdge(0, 1);
+            service.AddEdge(3, 3);
+            service.AddEdge(2, 3);
+            var expectedTraversal = new List<int>(new int[] { 2, 0, 3, 1 });
+
+            var result = service.BreadthFirstSearch(2);
+
+            Assert.AreEqual(expectedTraversal, result);
+        }
+    }
+}
