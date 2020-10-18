9070bd6ac2fcca0c06f0475bf0537fc3880e8c3e
EmmaLeu <manuela.leu@uipath.com>
2020-10-18 15:53:45 +0300

    Given a balanced search tree, return for each level a LinkedList containing the nodes on that level

--- AlgorithmsPractice/TreesAndGraphs/BinarySearchTreeService.cs
+++ AlgorithmsPractice/TreesAndGraphs/BinarySearchTreeService.cs
@@ -1,9 +1,7 @@
-﻿namespace AlgorithmsPractice.TreesAndGraphs
+﻿using System.Collections.Generic;
+
+namespace AlgorithmsPractice.TreesAndGraphs
 {
-    /// <summary>
-    /// Given a sorted (increasing order) array, 
-    /// write an algorithm to create a binary tree with minimal height 
-    /// </summary>
     public class BinarySearchTreeService
     {
         private static BinaryNode BuildFromSortedArray(int[] values, int start, int end)
@@ -21,6 +19,10 @@
             return root;
         }
 
+        /// <summary>
+        /// Given a sorted (increasing order) array, 
+        /// write an algorithm to create a binary tree with minimal height 
+        /// </summary>
         public static BinaryNode BuildMinimalHeightTreeFromSortedArray(int[] values)
         {
             if(values == null || values.Length == 0)
@@ -28,6 +30,58 @@
                 return null;
             }
             return BuildFromSortedArray(values, 0, values.Length - 1);
+        }
+
+        ///<summary>
+        ///Given a binary search tree, design an algorithm which creates a linked list of all the nodes at each depth
+        ///(eg, if you have a tree with depth D, you’ll have D linked lists)
+        ///</summary>
+        public static Dictionary<int, LinkedList<int>> GetNodeListsByLevel(BinaryNode node)
+        {
+            if(node == null)
+            {
+                return null;
+            }
+
+            var nodeListsByLevel = new Dictionary<int, LinkedList<int>>();
+            var queue = new Queue<BinaryNode>();
+
+            queue.Enqueue(node);
+            queue.Enqueue(null); //level delimiter
+
+            var level = 0;
+            nodeListsByLevel[0] = new LinkedList<int>();
+
+            while(queue.Count != 0)
+            {
+                node = queue.Dequeue();
+                if (node == null) //cosumed level, time to upgrade
+                {
+                    if (queue.Count != 0)
+                    {
+                        queue.Enqueue(null); //but first, delimit new level
+                        nodeListsByLevel[level + 1] = new LinkedList<int>();
+
+                    }
+
+                    level++;             
+                }
+                else
+                {
+                    nodeListsByLevel[level].AddLast(node.Value);
+
+                    if (node.Left != null)
+                    {
+                        queue.Enqueue(node.Left);
+                    }
+                    if (node.Right != null)
+                    {
+                        queue.Enqueue(node.Right);
+                    }
+                }
+            }
+
+            return nodeListsByLevel;
         }
     }
 }
--- AlgorithmsPracticeTests/TreesAndGraphs/BinarySearchTreeServiceTests.cs
+++ AlgorithmsPracticeTests/TreesAndGraphs/BinarySearchTreeServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using AlgorithmsPractice.TreesAndGraphs;
+using System.Collections.Generic;
 
 namespace AlgorithmsPracticeTests.TreesAndGraphs
 {
@@ -39,6 +40,24 @@ namespace AlgorithmsPracticeTests.TreesAndGraphs
             var node = BinarySearchTreeService.BuildMinimalHeightTreeFromSortedArray(values);
 
             Assert.AreEqual(1, node.Value);
+        }
+
+        [Test]
+        public void GetNodesByLevel_Test()
+        {
+            var tree = BinarySearchTreeService.BuildMinimalHeightTreeFromSortedArray(new[] { 1, 2, 3, 4, 5, 6, 7, 8 });
+
+            var result = BinarySearchTreeService.GetNodeListsByLevel(tree);
+
+            var expected = new Dictionary<int, LinkedList<int>>()
+            {
+                { 0, new LinkedList<int>(new[] { 4 })},
+                { 1, new LinkedList<int>(new[] { 2, 6 }) },
+                { 2, new LinkedList<int>(new[] { 1, 3, 5, 7 }) },
+                { 3, new LinkedList<int>(new[] { 8 }) }
+            };
+
+            Assert.AreEqual(expected, result);
         }
     }
 }
