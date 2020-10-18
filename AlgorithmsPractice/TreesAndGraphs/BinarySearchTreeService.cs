fcff3656aa0cce28d4588e4f9c6405988189fad7
EmmaLeu <manuela.leu@uipath.com>
2020-10-18 14:10:46 +0300

    Create minimum height binary search tree from sorted array

---
+++ AlgorithmsPractice/TreesAndGraphs/BinarySearchTreeService.cs
@@ -0,0 +1,33 @@
+﻿namespace AlgorithmsPractice.TreesAndGraphs
+{
+    /// <summary>
+    /// Given a sorted (increasing order) array, 
+    /// write an algorithm to create a binary tree with minimal height 
+    /// </summary>
+    public class BinarySearchTreeService
+    {
+        private static BinaryNode BuildFromSortedArray(int[] values, int start, int end)
+        {
+            if(values == null || values.Length == 0 || start > end)
+            {
+                return null;
+            }
+
+            var middle = (start + end) / 2;
+            var rootValue = values[middle];
+            var root = new BinaryNode(rootValue);
+            root.Left = BuildFromSortedArray(values, start, middle - 1);
+            root.Right = BuildFromSortedArray(values, middle + 1, end);
+            return root;
+        }
+
+        public static BinaryNode BuildMinimalHeightTreeFromSortedArray(int[] values)
+        {
+            if(values == null || values.Length == 0)
+            {
+                return null;
+            }
+            return BuildFromSortedArray(values, 0, values.Length - 1);
+        }
+    }
+}
---
+++ AlgorithmsPracticeTests/TreesAndGraphs/BinarySearchTreeServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using AlgorithmsPractice.TreesAndGraphs;
+
+namespace AlgorithmsPracticeTests.TreesAndGraphs
+{
+    public class BinarySearchTreeServiceTests
+    {
+        [Test]
+        public void BuildMinimalHeightTreeFromSortedArray_Length8_Test()
+        {
+            var values = new[] { 1, 2, 3, 4, 5, 6, 7, 8};
+            var node = BinarySearchTreeService.BuildMinimalHeightTreeFromSortedArray(values);
+
+            Assert.AreEqual(4, node.Value);
+
+            Assert.AreEqual(2, node.Left.Value);
+            Assert.AreEqual(1, node.Left.Left.Value);
+            Assert.AreEqual(3, node.Left.Right.Value);
+
+            Assert.AreEqual(6, node.Right.Value);
+            Assert.AreEqual(5, node.Right.Left.Value);
+            Assert.AreEqual(7, node.Right.Right.Value);
+            Assert.AreEqual(8, node.Right.Right.Right.Value);
+        }
+
+        [Test]
+        public void BuildMinimalHeightTreeFromSortedArray_Null_Test()
+        {
+            int[] values = null;
+            var node = BinarySearchTreeService.BuildMinimalHeightTreeFromSortedArray(values);
+
+            Assert.Null(node);
+        }
+
+        [Test]
+        public void BuildMinimalHeightTreeFromSortedArray_Length1_Test()
+        {
+            int[] values = { 1 };
+            var node = BinarySearchTreeService.BuildMinimalHeightTreeFromSortedArray(values);
+
+            Assert.AreEqual(1, node.Value);
+        }
+    }
+}
