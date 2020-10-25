84245b5356b6c06def8903b1a0d9f150d5d98c3f
EmmaLeu <manuela.leu@uipath.com>
2020-10-25 13:41:28 +0200

    Determine all paths (not necessarily starting from root) in a binary tree that sum up to a given value

--- AlgorithmsPractice/TreesAndGraphs/BinaryTreeService.cs
+++ AlgorithmsPractice/TreesAndGraphs/BinaryTreeService.cs
@@ -99,6 +99,74 @@ namespace AlgorithmsPractice.TreesAndGraphs
             }
         }
 
+        public static bool IsSubtreeOf(BinaryNode t1, BinaryNode t2)
+        {
+            var inorderT2 = new StringBuilder();
+            InOrder(t2, inorderT2);
+
+            var inorderT1 = new StringBuilder();
+            InOrder(t1, inorderT1);
+
+            if (!inorderT1.ToString().Contains(inorderT2.ToString()))
+            {
+                return false;
+            }
+
+            var preorderT2 = new StringBuilder();
+            InOrder(t2, preorderT2);
+
+            var preorderT1 = new StringBuilder();
+            InOrder(t1, preorderT1);
+
+            if (!preorderT1.ToString().Contains(preorderT2.ToString()))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static List<List<int>> GetSumPaths(BinaryNode node, int sum)
+        {
+            var paths = new List<List<int>>();
+            FindSum(node, sum, new List<int>(), 0, paths);
+            return paths;
+        }
+
+        private static void FindSum(BinaryNode node, int sum, List<int> buffer, int level, List<List<int>> paths)
+        {
+            if(node == null)
+            {
+                return;
+            }
+
+            var temporarySum = sum;
+            buffer.Add(node.Value);
+
+            for (var index = level; index > -1; index--)
+            {
+                temporarySum -= buffer[index];
+                if(temporarySum == 0)
+                {
+                    paths.Add(GetPath(buffer, index, level));
+                }
+            }
+
+            FindSum(node.Left, sum, buffer.ToList(), level + 1, paths);
+            FindSum(node.Right, sum, buffer.ToList(), level + 1, paths);
+        }
+
+        private static List<int> GetPath(List<int> buffer, int startIndex, int endIndex)
+        {
+            var path = new List<int>();
+            for(var index = startIndex; index <= endIndex; index++)
+            {
+                path.Add(buffer[index]);
+            }
+
+            return path;
+        }
+
         private static int GetContainedNodes(BinaryNode root, BinaryNode node1, BinaryNode node2)
         {
             int foundNodes = 0;
@@ -161,33 +229,6 @@ namespace AlgorithmsPractice.TreesAndGraphs
             {
                 InOrder(root.Right, traversal);
             }
-        }
-
-        public static bool IsSubtreeOf(BinaryNode t1, BinaryNode t2)
-        {
-            var inorderT2 = new StringBuilder();
-            InOrder(t2, inorderT2);
-
-            var inorderT1 = new StringBuilder();
-            InOrder(t1, inorderT1);
-
-            if(!inorderT1.ToString().Contains(inorderT2.ToString()))
-            {
-                return false;
-            }
-
-            var preorderT2 = new StringBuilder();
-            InOrder(t2, preorderT2);
-
-            var preorderT1 = new StringBuilder();
-            InOrder(t1, preorderT1);
-
-            if (!preorderT1.ToString().Contains(preorderT2.ToString()))
-            {
-                return false;
-            }
-
-            return true;
         }
     }
 }
--- AlgorithmsPracticeTests/TreesAndGraphs/BinaryTreeServiceTests.cs
+++ AlgorithmsPracticeTests/TreesAndGraphs/BinaryTreeServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using AlgorithmsPractice.TreesAndGraphs;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace AlgorithmsPracticeTests.TreesAndGraphs
 {
@@ -8,7 +9,7 @@ namespace AlgorithmsPracticeTests.TreesAndGraphs
         [Test]
         public void FindFirstCommonAncestor_WithParentLink_Test()
         {
-            var testCases = GetTestCases();
+            var testCases = GetCommonAncestorTestCases();
             
             foreach(var testCase in testCases)
             {
@@ -19,72 +20,12 @@ namespace AlgorithmsPracticeTests.TreesAndGraphs
         [Test]
         public void FindFirstCommonAncestor_WithoutParentLink_Test()
         {
-            var testCases = GetTestCases();
+            var testCases = GetCommonAncestorTestCases();
 
             foreach (var testCase in testCases)
             {
                 Assert.AreEqual(testCase.FirstCommonAncestor, BinaryTreeService.FindFirstCommonAncestor(testCase.Root, testCase.Node1, testCase.Node2));
             }
-        }
-
-        private static TestCase[] GetTestCases()
-        {
-            var root = new BinaryNodeWithParent(1);
-            var leftLeft = new BinaryNodeWithParent(4);
-            var leftRight = new BinaryNodeWithParent(5);
-
-            var left = new BinaryNodeWithParent(2)
-            {
-                Parent = root,
-                Left = leftLeft,
-                Right = leftRight
-            };
-
-            leftLeft.Parent = left;
-            leftRight.Parent = left;
-
-            var right = new BinaryNodeWithParent(3)
-            {
-                Parent = root
-            };
-
-            root.Left = left;
-            root.Right = right;
-
-            return new[]
-            {
-                new TestCase
-                {
-                    Root = root,
-                    Node1 = leftRight,
-                    Node2 = right,
-                    FirstCommonAncestor = root
-                },
-
-                new TestCase
-                {
-                    Root = root,
-                    Node1 = leftLeft,
-                    Node2 = leftRight,
-                    FirstCommonAncestor = left
-                },
-
-                new TestCase
-                {
-                    Root = root,
-                    Node1 = root,
-                    Node2 = leftRight,
-                    FirstCommonAncestor = root
-                },
-            };
-        }
-
-        private class TestCase
-        {
-            public BinaryNodeWithParent Root { get; set; }
-            public BinaryNodeWithParent Node1 { get; set; }
-            public BinaryNodeWithParent Node2 { get; set; }
-            public BinaryNodeWithParent FirstCommonAncestor { get; set; }
         }
 
         [Test]
@@ -96,7 +37,16 @@ namespace AlgorithmsPracticeTests.TreesAndGraphs
             }
         }
 
-        private SubTreeTestCase[] GetSubTreeTestCases()
+        [Test]
+        public void FindSum_Test()
+        {
+            foreach (var tc in GetFindSumTestCases())
+            {
+                Assert.AreEqual(tc.ExpectedPaths, BinaryTreeService.GetSumPaths(tc.Root, tc.Sum));
+            }
+        }
+
+        private static SubTreeTestCase[] GetSubTreeTestCases()
         {
             var firstTree = new BinaryNode(1);
             var left = new BinaryNode(2);
@@ -136,6 +86,113 @@ namespace AlgorithmsPracticeTests.TreesAndGraphs
             };
         }
 
+        private static CommonAncestorTestCase[] GetCommonAncestorTestCases()
+        {
+            var root = new BinaryNodeWithParent(1);
+            var leftLeft = new BinaryNodeWithParent(4);
+            var leftRight = new BinaryNodeWithParent(5);
+
+            var left = new BinaryNodeWithParent(2)
+            {
+                Parent = root,
+                Left = leftLeft,
+                Right = leftRight
+            };
+
+            leftLeft.Parent = left;
+            leftRight.Parent = left;
+
+            var right = new BinaryNodeWithParent(3)
+            {
+                Parent = root
+            };
+
+            root.Left = left;
+            root.Right = right;
+
+            return new[]
+            {
+                new CommonAncestorTestCase
+                {
+                    Root = root,
+                    Node1 = leftRight,
+                    Node2 = right,
+                    FirstCommonAncestor = root
+                },
+
+                new CommonAncestorTestCase
+                {
+                    Root = root,
+                    Node1 = leftLeft,
+                    Node2 = leftRight,
+                    FirstCommonAncestor = left
+                },
+
+                new CommonAncestorTestCase
+                {
+                    Root = root,
+                    Node1 = root,
+                    Node2 = leftRight,
+                    FirstCommonAncestor = root
+                },
+            };
+        }
+
+        private static FindSumTestCase[] GetFindSumTestCases()
+        {
+            var root = new BinaryNode(1); //L0
+
+            var left = new BinaryNode(3);
+            var right = new BinaryNode(-1);
+
+            var leftLeft = new BinaryNode(2);
+            var leftRight = new BinaryNode(1);
+            var leftRightLeft = new BinaryNode(1);
+
+            var rightLeft = new BinaryNode(4);
+            var rightLeftLeft = new BinaryNode(1);
+            var rightLeftRight = new BinaryNode(2);
+            var rightRight= new BinaryNode(5);
+            var rightRightRight = new BinaryNode(6);
+
+            //L1
+            root.Left = left; 
+            root.Right = right;
+
+            //L2
+            left.Left = leftLeft;
+            left.Right = leftRight;
+
+            right.Left = rightLeft;
+            right.Right = rightRight;
+
+            //L3
+            left.Right.Left = leftRightLeft;
+            right.Left.Left = rightLeftLeft;
+            right.Left.Right = rightLeftRight;
+            right.Right.Right = rightRightRight;
+
+            return new FindSumTestCase[]
+            {
+                new FindSumTestCase
+                {
+                    Root = root,
+                    Sum = 5,
+                    ExpectedPaths = new List<List<int>>
+                    {
+                        new List<int>{ 3, 2 },
+                        new List<int>{ 1, 3, 1 },
+                        new List<int>{ 3, 1, 1 },
+                        new List<int>{ 4, 1 },
+                        new List<int>{ 1, -1, 4, 1 },
+                        new List<int>{ -1, 4, 2 },
+                        new List<int>{ 5 },
+                        new List<int>{ 1, -1, 5 }
+                    }
+                }
+            };
+        }
+
         private class SubTreeTestCase
         {
             public BinaryNode FirstTree { get; set; }
@@ -143,6 +200,21 @@ namespace AlgorithmsPracticeTests.TreesAndGraphs
             public BinaryNode SecondTree { get; set; }
 
             public bool IsSubtreeOf { get; set; }
+        }
+
+        private class CommonAncestorTestCase
+        {
+            public BinaryNodeWithParent Root { get; set; }
+            public BinaryNodeWithParent Node1 { get; set; }
+            public BinaryNodeWithParent Node2 { get; set; }
+            public BinaryNodeWithParent FirstCommonAncestor { get; set; }
+        }
+
+        private class FindSumTestCase
+        {
+            public BinaryNode Root { get; set; }
+            public int Sum { get; set; }
+            public List<List<int>> ExpectedPaths { get; set; }
         }
     }
 }
