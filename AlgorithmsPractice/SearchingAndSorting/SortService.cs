95d252715432e97a70420c71a07778a5fcf0eeb0
EmmaLeu <manuela.leu@uipath.com>
2020-10-29 00:23:09 +0200

    Merge 2 sorted arrays

---
+++ AlgorithmsPractice/SearchingAndSorting/SortService.cs
@@ -0,0 +1,36 @@
+﻿namespace AlgorithmsPractice.SearchingAndSorting
+{
+    public class SortService
+    {
+        /// <summary>
+        /// Merge 2 sorted arrays knowing that the first one has a buffer for the second one's length
+        /// </summary>
+        /// <param name="a">First array (the longer one)</param>
+        /// <param name="b">Second array</param>
+        /// <param name="aElementNumber">Number of occupied positions in a</param>
+        /// <param name="bElementNumber">Number of occupied positions in b</param>
+        public static void MergeSortedArrays(int[] a, int[] b, int aElementNumber, int bElementNumber)
+        {
+            int lastAPosition = a.Length - 1;
+            var lastBIndex = bElementNumber - 1;
+            var lastAIndex = aElementNumber - 1;
+
+            while(lastBIndex >= 0 && lastAIndex >= 0)
+            {
+                if (a[lastAIndex] > b[lastBIndex])
+                {
+                    a[lastAPosition--] = a[lastAIndex--];
+                }
+                else
+                {
+                    a[lastAPosition--] = b[lastBIndex--];
+                }
+            }
+
+            while(lastBIndex >= 0)
+            {
+                a[lastAPosition--] = b[lastBIndex--];
+            }
+        }
+    }
+}
---
+++ AlgorithmsPracticeTests/SearchingAndSorting/SortServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using AlgorithmsPractice.SearchingAndSorting;
+using NUnit.Framework;
+
+namespace AlgorithmsPracticeTests.SearchingAndSorting
+{
+    public class SortServiceTests
+    {
+        [Test]
+        public void MergeArraysTest_Intertwined_Test()
+        {
+            var a = new[]{ 1, 7, 10, 15, 19, 0, 0, 0, 0 };
+            var b = new[] { 2, 8, 20, 21 };
+            SortService.MergeSortedArrays(a, b, 5, 4);
+            Assert.AreEqual(new[] { 1, 2, 7, 8, 10, 15, 19, 20, 21 }, a);
+        }
+
+        [Test]
+        public void MergeArraysTest_NotIntertwined_bLast_Test()
+        {
+            var a = new[] { 1, 7, 10, 15, 19, 0, 0, 0, 0 };
+            var b = new[] { 20, 21, 29, 30 };
+            SortService.MergeSortedArrays(a, b, 5, 4);
+            Assert.AreEqual(new[] { 1, 7, 10, 15, 19, 20, 21, 29, 30 }, a);
+        }
+
+        [Test]
+        public void MergeArraysTest_NotIntertwined_aLast_Test()
+        {
+            var a = new[] { 19, 20, 21, 29, 30, 0, 0, 0, 0 };
+            var b = new[] { 1, 2, 3, 4 };
+            SortService.MergeSortedArrays(a, b, 5, 4);
+            Assert.AreEqual(new[] { 1, 2, 3, 4, 19, 20, 21, 29, 30 }, a);
+        }
+    }
+}
